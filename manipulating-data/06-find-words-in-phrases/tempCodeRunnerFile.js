3a9a5ec79617e1716be16ba9f1daa49dcec5b55e
AnaClaudiaSac <anaclaudiasacute75@gmail.com>
2025-04-08 15:40:29 -0300

    Finished the find words in phrases exercises

---
+++ manipulating-data/06-find-words-in-phrases/tempCodeRunnerFile.js
@@ -0,0 +1,13 @@
+function endsWithWord(phrase, word) {
+//   return phrase.endsWith(word);
+// }
+
+// console.log(endsWithWord("Hoje é segunda-feira", "segunda-feira")); 
+// console.log(endsWithWord("Hoje é segunda-feira", "domingo")); 
+// console.log(endsWithWord("Adoro viajar e conhecer lugares novos", "novos"));
+// console.log(
+//   endsWithWord(
+//     "Amo comer Waffles no café da manhã com doce de leite!",
+//     "Waffles"
+//   )
+// );
